8203fc0ea15d15c800cae31eab90bdeabbb954d4
Nicolas A Perez <anicolaspp@gmail.com>
2020-11-14 12:48:26 -0500

    Adding comments

--- pkg/source/source.go
+++ pkg/source/source.go
@@ -9,10 +9,14 @@ func From(data []interface{}) stage.Composable {
 	return s
 }
 
-func FromIterator(it *stage.Iterator) stage.Composable  {
+// FromIterator creates a Composable using an Iterator
+func FromIterator(it *stage.Iterator) stage.Composable {
 	return stage.NewComposableFromIterator(it)
 }
 
+// FromGenerator creates a Composable using an initial value seed, and a function succ that generates
+// the next value.
+// Notice that the values are generated lazily.
 func FromGenerator(seed interface{}, succ stage.Succ) stage.Composable {
 	return stage.NewComposableFromIterator(stage.NewGenerator(seed, succ))
 }
